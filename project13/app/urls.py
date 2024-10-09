c87c67bf642b59abdd8387e9f8c1b8992e2d1f78
likith1003 <123715227+likith1003@users.noreply.github.com>
2024-10-09 18:39:57 +0530

    Add files via upload

--- project13/app/urls.py
+++ project13/app/urls.py
@@ -5,4 +5,7 @@ from .views import *
 urlpatterns = [
     path('', home, name='home'),
     path('register', register, name='register'),
+    path('user_login', user_login, name='user_login'),
+    path('user_logout', user_logout, name='user_logout'),
+    path('display_profile', display_profile, name='display_profile')
 ]
--- project13/app/views.py
+++ project13/app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .forms import *
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core.mail import send_mail
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -9,24 +14,60 @@ def home(request):
 
 def register(request):
     EUFO = UserForm()
-    d = {'EUFO': EUFO}
-    if request.method == 'POST':
+    EPFO = ProfileForm()
+    d = {'EUFO': EUFO, 'EPFO': EPFO}
+    if request.method == 'POST' and request.FILES:
         UFDO = UserForm(request.POST)
-        if UFDO.is_valid():
+        PFDO = ProfileForm(request.POST, request.FILES)
+        if UFDO.is_valid() and PFDO.is_valid():
             pw = UFDO.cleaned_data.get('password')
             MUFDO = UFDO.save(commit=False)
             MUFDO.set_password(pw)
-            email = UFDO.cleaned_data.get('email')
-            message = f"Hello {UFDO.cleaned_data.get('first_name')},\n Thank you for Registration towards our Application \n Thanks & Regards Team A1"
-            send_mail(
-                "Regustration Successfull",
-                message,
-                'likith.qsp@gmail.com',
-                [email, 'luckeylikith@gmail.com', 'smarakidash1003@gmail.com', 'khuntiachandan26@gmail.com'],
-                fail_silently=False 
-            )
+            MPFDO = PFDO.save(commit=False)
+            MPFDO.username = MUFDO
+            # email = UFDO.cleaned_data.get('email')
+            # message = f"Hello {UFDO.cleaned_data.get('first_name')},\n Thank you for Registration towards our Application \n Thanks & Regards Team A1"
+            # send_mail(
+            #     "Regustration Successfull",
+            #     message,
+            #     'likith.qsp@gmail.com',
+            #     [email, 'luckeylikith@gmail.com', 'smarakidash1003@gmail.com', 'khuntiachandan26@gmail.com'],
+            #     fail_silently=False 
+            # )
             MUFDO.save()
+            MPFDO.save()
             return HttpResponse('User Registration Is successfull')
         return HttpResponse('Invaalid data')
 
     return render(request, 'register.html', d)
+
+
+def user_login(request):
+    if request.method == 'POST':
+        un = request.POST.get('un')
+        pw = request.POST.get('pw')
+        AUO = authenticate(username=un, password=pw)
+        if AUO and AUO.is_active:
+            login(request, AUO)
+            request.session['username'] = un
+            d = {'user': AUO}
+            return HttpResponseRedirect(reverse('home'))
+        return HttpResponse('invalid Creds')
+    return render(request, 'user_login.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
+
+
+@login_required
+def display_profile(request):
+    un = request.session.get('username')
+    if un:
+        UO = User.objects.get(username=un)
+        PO = Profile.objects.filter(username=UO)[0]
+        d = {'user': UO, 'po': PO}
+
+        return render(request, 'display_profile.html', d)
+    return render(request, 'display_profile.html')
